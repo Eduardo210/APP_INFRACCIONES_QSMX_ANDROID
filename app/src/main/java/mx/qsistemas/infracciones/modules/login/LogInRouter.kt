bbbaf8a75b7c0255f299bee05b0f753ebfe2b033
Omar Zúñiga <ozuniga@qsistemas.mx>
2019-06-13 10:22:34 -0500

    * Create report package * Search report package * Main activity

--- app/src/main/java/mx/qsistemas/infracciones/modules/login/LogInRouter.kt
+++ app/src/main/java/mx/qsistemas/infracciones/modules/login/LogInRouter.kt
@@ -1,9 +1,13 @@
 package mx.qsistemas.infracciones.modules.login
 
+import android.content.Intent
+import mx.qsistemas.infracciones.Application
+import mx.qsistemas.infracciones.modules.main.MainActivity
+
 class LogInRouter(private val activity: LogInActivity) : LogInContracts.Router {
 
     override fun presentMainActivity() {
-        /*activity.startActivity(Intent(Application.getContext(), MainActivity::class.java))
-        activity.finish()*/
+        activity.startActivity(Intent(Application.getContext(), MainActivity::class.java))
+        activity.finish()
     }
 }

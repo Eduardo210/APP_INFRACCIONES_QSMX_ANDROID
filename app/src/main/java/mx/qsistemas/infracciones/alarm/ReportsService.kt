14ce9c0bbd0c0e2939e204dcff70519a364f4a2d
Omar Zúñiga <ozuniga@qsistemas.mx>
2019-07-17 17:17:34 -0500

    * New: Firebase Push Notifications * New: Firebase Upload Database * Correction:  Ime Action Done Item Motivation

--- app/src/main/java/mx/qsistemas/infracciones/alarm/ReportsService.kt
+++ app/src/main/java/mx/qsistemas/infracciones/alarm/ReportsService.kt
@@ -53,8 +53,8 @@ class ReportsService : JobService() {
                     val motivationList = SendInfractionManager.getInfractionMotivationList(it.id.toLong())
                     val motivationListRequest = mutableListOf<SaveInfractionRequest.InfractionRequest.InfractionFractions_Request>()
                     /* Save the motivation list into the request */
-                    motivationList.forEach {
-                        motivationListRequest.add(SaveInfractionRequest.InfractionRequest.InfractionFractions_Request(it.id_fraction, it.penalty_points, it.salary, it.motivation))
+                    motivationList.forEach { x ->
+                        motivationListRequest.add(SaveInfractionRequest.InfractionRequest.InfractionFractions_Request(x.id_fraction, x.penalty_points, x.salary, x.motivation))
                     }
                     /* Get offender address */
                     val personAddress = SendInfractionManager.getPersonAddress(it.id.toLong())
